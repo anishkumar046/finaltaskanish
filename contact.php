03cbb30984e16a864667cd6f2aa032806dc09121
anishkumar046 <gecvaishali91@gmail.com>
2025-08-01 20:51:19 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -10,9 +10,10 @@
 
   <section>
     <h2>Contact Us</h2>
-    <p>Email: anish@gmail.com</p>
-    <p>Phone: 01234-567890</p>
+    <p>Email: anishgupta882575@gmail.com</p>
+    <p>Phone: 9162681286</p>
     <p>Address: Computer Science Department, GEC Vaishali</p>
+     <p>Created by: Anish kumar</p>
   </section>
 
   <?php include 'includes/footer.php'; ?>
